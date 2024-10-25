541462b76637b7808719ef1f8da2cdebce6461c2
niharika sahu <121661609+nihu-13@users.noreply.github.com>
2024-10-25 22:00:23 +0530

    HomePage Initialize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component, HostListener } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, LandingPageComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'homePage01';
+  
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    const navbar = document.querySelector('.navbar') as HTMLElement;
+    if (window.pageYOffset > 100) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  }
 }
