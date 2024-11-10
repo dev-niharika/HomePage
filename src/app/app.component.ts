044568d689e1ac7a71cf828ddd058f3e31fde2fc
niharika sahu <121661609+nihu-13@users.noreply.github.com>
2024-11-10 16:03:42 +0530

    about section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component, HostListener } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutComponent } from './about/about.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink,LandingPageComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink,LandingPageComponent,AboutComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
    
-    { path: '', component: LandingPageComponent }
+    { path: '', component: LandingPageComponent },
+    { path: 'about', component: AboutComponent }
 ];
 
 @NgModule({
