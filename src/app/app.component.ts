21b2dab8dfd651f72297f305006c1feadd8f1c77
niharika sahu <121661609+nihu-13@users.noreply.github.com>
2024-11-13 20:53:12 +0530

    about section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component} from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { AboutComponent } from './about/about.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink,LandingPageComponent,AboutComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutComponent } from './about/about.component';
+import { ProjectComponent } from './project/project.component';
+import { TeamComponent } from './team/team.component';
+import { ServiceComponent } from './service/service.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
    
     { path: '', component: LandingPageComponent },
-    { path: 'about', component: AboutComponent }
+    { path: 'about', component: AboutComponent },
+    { path: 'project', component: ProjectComponent },
+    { path: 'team', component: TeamComponent },
+    { path: 'service', component: ServiceComponent },
+    { path: 'contact', component: ContactComponent }
 ];
 
 @NgModule({
